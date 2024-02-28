a8a8976107d9e5a67d7ce60f75cb83fb0a5510e8
daffa tabiano <daffatabianoo@gmail.com>
2024-02-28 19:31:01 +0700

    menambahkan file (about.css logoFooter.png, component Footer.jsx, footer.css), mengubah style about dan menambahkan paragraf about.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,32 @@
-import Navbar from "../components/Navbar";
-
+import "../about.css";
+import Footer from "../components/Footer";
 const About = () => {
     return (
-        <div className="aboutHomepage d-flex text-center">
-            <Navbar />
-            <img className="imageHeader img-fluid d-block" src={"https://th.bing.com/th/id/OIG2.ZL5vpPf5ibt4izl0cZWo?pid=ImgGn"} alt="" />
-        
-                <h1 className="position-absolute" >🌵Welcome to Our Saloon🌵</h1>
-                {/* <img className="imageAbout" src={"https://th.bing.com/th/id/OIG2.RRH_P7CbNXQN6LsZN8PX?pid=ImgGn"} alt="" /> */}
-                <div className="contentAbout2">
-                    <h2 className="d-flex">Why You must Come ? <button type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">🤠</button></h2>
-                    <div style={{ height: "120px" }}>
-                        <div className="collapse collapse-horizontal" id="collapseWidthExample">
-                            <div className="card card-body" style={{ width: "300px" }}>
-                                This is some placeholder content for a horizontal collapse. It's hidden by default and shown when triggered.
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div>
+            <header className="masthead">
+                <p className="masthead-intro">Hii..</p>
+                <h1 className="masthead-heading">Welcome To Our Saloon Cafè</h1>
+            </header>
+            <section className="intro">
+                <h1>Introduction</h1>
+                <p>I'm a future Front End developer who loves <span className="bold">good design</span>, <span className="bold">good food</span> and <span className="bold">good people</span>. I work in totally different industry now, but I am now in trasition of my new career!. I am so excited about learning that make interactive webpage and design.</p>
+                <p>I love the web and all the awesome new technologies. I'm excited to learn a new things that make the web more beautiful, functional, and perfect.</p>
+
+                <p>I love learning a new thing! Expecially in tech industries. I am so happy that I can learn at Thinkful and as student of Thinkful community!</p>
+            </section>
+            <section className="more-about-me">
+                <h1>More About Me</h1>
+                <h2>What are your favorite hobbies?</h2>
+                <p>My favorite hobby was playing Counter-Strike. But now I am enjoying most of my time with study coding!</p>
+                <h2>What's your dream job?</h2>
+                <p>My goal is that become a full stack developer, so that I can manage front-end side and back-end side as well.
+                    Before achieve that goal, I need to master front-end developer skills first!</p>
+                <h2>What's your background?</h2>
+                <p>I've seen programming since I was very young. My dad was web programmer!. He always wants me to do stuying web programming. Here I am! now I am studying hard to master front end developing skills.</p>
+                <h2>What music have you been listening to lately?</h2>
+                <p>I've been listing to a ton of K-Pop lately, obviously I am korean. Nowaday K-Pop is very popular even in the United States.</p>
+            </section>
+            <Footer />
         </div>
     )
 }
